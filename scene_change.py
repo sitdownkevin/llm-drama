37c7c2e588ba4e671c7711a9a9704aec4a0a0352
Ke Xu <kexu567@gmail.com>
2025-05-17 18:44:08 +0800

    Update code

---
+++ scene_change.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv, find_dotenv
+import asyncio
+
+load_dotenv(find_dotenv())
+
+from langchain_openai import ChatOpenAI
+
+with open('prompts/scene_change.xml', 'r') as f:
+    prompt_scene_change = f.read()
+
+
+async def main():
+    llm = ChatOpenAI(model="gpt-4.1", temperature=0)
+    result = await llm.ainvoke(prompt_scene_change)
+    print(result)
+    
+    with open('export/scene_change.md', 'w') as f:
+        f.write(result.content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
